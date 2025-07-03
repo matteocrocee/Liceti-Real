1f499f7e1446f05957762fce2e517c02bf6b562b
Fedeeee276 <fedebozzetto@gmail.com>
2025-07-03 09:37:21 +0200

    moneta+gamemanager

---
+++ Liceti3D/Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public int punteggio = 0;
+    public TextMeshProUGUI punteggioText;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AggiungiPunti(int valore)
+    {
+        punteggio += valore;
+        AggiornaUI();
+    }
+
+    private void AggiornaUI()
+    {
+        if (punteggioText != null)
+        {
+            punteggioText.text = "Punti: " + punteggio;
+        }
+    }
+}
--- Liceti3D/Assets/coin.cs
+++ Liceti3D/Assets/coin.cs
@@ -1,44 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    public float rotationSpeed = 90f; // Rotazione in gradi al secondo
-    public AudioClip pickupSound;     // (Facoltativo) suono alla raccolta
-    public GameObject pickupEffect;   // (Facoltativo) effetto visivo
-    private bool collected = false;
+    public int valore = 1; // valore della moneta
 
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        // Ruota continuamente la moneta
-        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        // Assicurati che solo il player possa raccoglierla
-        if (!collected && other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            collected = true;
+            GameManager.Instance.AggiungiPunti(valore); // 👈 Chiama il GameManager
 
-            // Aggiungi punti o logica al contatore
-            CoinCollector.Instance.AddCoin();
-
-            // Effetto visivo opzionale
-            if (pickupEffect != null)
-            {
-                Instantiate(pickupEffect, transform.position, Quaternion.identity);
-            }
-
-            // Suono opzionale
-            if (pickupSound != null)
-            {
-                AudioSource.PlayClipAtPoint(pickupSound, transform.position);
-            }
-
-            // Distruggi la moneta
-            Destroy(gameObject);
+            Destroy(gameObject); // distruggi la moneta
         }
     }
 }
